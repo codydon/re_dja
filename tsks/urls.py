c7743147b794e43a18ad1a0d525d2f0ea10cc4e9
codydon <donaldonserio20@gmail.com>
2023-02-18 12:28:10 +0300

    delete commit

--- tsks/urls.py
+++ tsks/urls.py
@@ -5,10 +5,8 @@ from . import views
 app_name = 'tsks'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:t_id>/', views.t_detail, name='t_detail'),
-    path('all/', views.all, name='all'),
-    path('delete/', views.delete_tasks, name='delete_tasks')
-    # path('<int:question_id>/', views.details, name='details')
+    path('add', views.add, name='add'),
+    path('<int:taskID>/delete/', views.delete, name='delete')
     # path('<int:question_id>/', views.details, name='details')
 ]
 
--- tsks/views.py
+++ tsks/views.py
@@ -5,10 +5,10 @@ from django.template import loader
 from django.http import Http404
 from django.urls import reverse
 from datetime import datetime
-
+from django.db import transaction
 
 # Create your views here.
-def all(request):
+def index(request):
     # return HttpResponse("This is the tsks index")
     latest_task_list = Task.objects.order_by('-task_name') #[:5]
     # output = ', '.join([q.task_name for q in latest_task_list])
@@ -21,23 +21,23 @@ def all(request):
     # return HttpResponse(template.render(context, request))
 
     context = {'latest_task_list': latest_task_list}
-    return render(request, 'tsks/all.html', context)
-
-def t_detail(request, t_id):
-    # try:
-    #     task = Task.objects.get(pk=t_id)
-    # except Task.DoesNotExist:
-    #     raise Http404("Task does not exist")
-    # return render(request, 'tsks/t_detail.html', {'task': task})
-
-    task = get_object_or_404(Task, pk=t_id)
-    return render(request, 'tsks/t_detail.html', {'task': task})
+    return render(request, 'tsks/index.html', context)
 
 # def t_detail(request, t_id):
-#     return HttpResponse("You're looking at question %s." % t_id)
+#     # try:
+#     #     task = Task.objects.get(pk=t_id)
+#     # except Task.DoesNotExist:
+#     #     raise Http404("Task does not exist")
+#     # return render(request, 'tsks/t_detail.html', {'task': task})
+
+#     task = get_object_or_404(Task, pk=t_id)
+#     return render(request, 'tsks/t_detail.html', {'task': task})
+
+# # def t_detail(request, t_id):
+# #     return HttpResponse("You're looking at question %s." % t_id)
 
 
-def index(request):
+def add(request):
     if request.method == 'POST':
         # Create a new instance of your model with data from the form
         new_obj = Task(
@@ -49,16 +49,48 @@ def index(request):
         new_obj.save()
         
         # Redirect the user to a success page or another relevant URL
-        return redirect('tsks:all')
+        return redirect('tsks:index')
     
     # If the request method is not POST, render the form template
     return render(request, 'tsks/index.html')
 
     # return HttpResponse("This is the tsks add")
 
-def delete_tasks(request):
-    if request.method == 'POST':
-        task_ids = request.POST.get('task_ids', '').split(',')
-        Task.objects.filter(id__in=task_ids).delete()
+def finish_tasks(request):
+    tasks = Task.objects.all()
 
-    return redirect('tsks:all')
+    if request.method == 'POST':
+        for task in tasks:
+            completed = request.POST.get('task-{}'.format(task.id), False)
+            task.completed = completed
+            task.save()
+
+    return render(request, 'tsks:index.html', {'tasks': tasks})
+
+def delete(request, taskID):
+    # # return HttpResponse("You're looking at question %s." % taskID)
+    # try:
+    #     with transaction.atomic():
+    #         # Find the task you want to delete
+    #         task = Task.objects.get(id=taskID)
+
+    #         # Delete the task
+    #         task.delete()
+
+    #     return render(request, 'tsks:index.html')
+    # except Task.DoesNotExist:
+    #     return HttpResponse("Item does not exist")
+    # # except Exception as e:
+    # #      return HttpResponse("Error deleting task", str(e))
+
+    try:
+        # Delete the task with ID 1
+        Task.objects.filter(id=taskID).delete()
+        return HttpResponseRedirect("/tsks")
+        
+
+    except Exception as e:
+        # Handle the exception
+        print("Error deleting task:", str(e))
+
+    
